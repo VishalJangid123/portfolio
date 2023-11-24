c5e43e2dbfcd46680d47b41532bc037c41be04b7
Vishal <vishaljangid2@outlook.com>
2023-11-24 14:31:32 +0700

    Update nextjs to allow github action to make basepath

--- next.config.js
+++ next.config.js
@@ -1,8 +1,20 @@
 /** @type {import('next').NextConfig} */
 
+const isGithubActions = process.env.GITHUB_ACTIONS || false
 const withMDX = require('@next/mdx')()
+
+let assetPrefix = ""
+let basePath = ""
+
+if (isGithubActions) {
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+
+  assetPrefix = `/${repo}/`
+  basePath = `/${repo}`
+}
 const nextConfig = {
-    basePath: '/portfolio',
+    assetPrefix: assetPrefix,
+    basePath: basePath,
     output: 'export',
     pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
     images: {
