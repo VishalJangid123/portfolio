a41322cf34eee284270c25a63bae3def8fed8a21
Vishal <vishaljangid2@outlook.com>
2024-01-05 23:30:42 +0700

    Update meta title and desc

--- app/layout.js
+++ app/layout.js
@@ -5,8 +5,8 @@ import Head from 'next/head';
 import Footer from '@/components/Footer/Footer';
 
 export const metadata = {
-  title: 'Vishal Jangid | Portfolio | Game Developer | Thailand | Unity Developer',
-  description: 'A Unity Game Developer with experience in Full stack development. I love to write code to create playable games with complex mechanics. I also enjoy working in full stack. My expertise lies within Unity game engine, and web apps and the main languages/frameworks in my tech stack are C#, ReactJS, and NodeJS. Training for Kickboxing and always ready to play 8-ball pool.',
+  title: 'Vishal Jangid | Game Developer | Unity Developer | Thailand',
+  description: 'Discover the portfolio of Vishal Jangid, a Unity Game Developer in Thailand. Expert in Unity, C#, ReactJS, and NodeJS. Crafting immersive games with creative coding finesse. Explore now!',
 }
 
 export default function RootLayout({ children }) {
@@ -31,11 +31,23 @@ export default function RootLayout({ children }) {
         <meta name="msapplication-TileColor" content="#ffffff" />
         <meta name="msapplication-TileImage" content="/ms-icon-144x144.png"></meta>
 
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://vishaljangid123.github.io/portfolio/" />
+        <meta property="og:title" content="Vishal Jangid | Game Developer | Unity Developer | Thailand" />
+        <meta property="og:description" content="Discover the portfolio of Vishal Jangid, a Unity Game Developer in Thailand. Expert in Unity, C#, ReactJS, and NodeJS. Crafting immersive games with creative coding finesse. Explore now!" />
+        <meta property="og:image" content="https://vishaljangid123.github.io/portfolio/Portfolio_SS.png" />
+
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://vishaljangid123.github.io/portfolio/" />
+        <meta property="twitter:title" content="Vishal Jangid | Game Developer | Unity Developer | Thailand" />
+        <meta property="twitter:description" content="Discover the portfolio of Vishal Jangid, a Unity Game Developer in Thailand. Expert in Unity, C#, ReactJS, and NodeJS. Crafting immersive games with creative coding finesse. Explore now!" />
+        <meta property="twitter:image" content="https://vishaljangid123.github.io/portfolio/Portfolio_SS.png" />
 
         <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet' />
         <link rel='stylesheet' href='https://unicons.iconscout.com/release/v4.0.0/css/line.css' ></link>
         <link href="https://fonts.googleapis.com/css?family=Poppins" rel="stylesheet"></link>
       </head>
+
       <body suppressContentEditableWarning={true}>
         <Header />
         {children}
