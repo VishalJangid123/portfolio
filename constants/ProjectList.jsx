090ce3085e93eb396f2867a3b436e70b838000f6
Vishal Jangid <vishaljangid2@outlook.com>
2025-01-29 01:33:30 +0700

    Project List

---
+++ constants/ProjectList.jsx
@@ -0,0 +1,18 @@
+export const ProjectList = 
+{
+    full_stack : [
+        "React"
+    ],
+    game_development : [
+        "ImageSwift",
+        "Opposites",
+        "Traffic Tango",
+        "Roller Fill",
+        "Drop the Box",
+        "VR Visualization of a Thai Historic temple",
+    ],
+    ai : [
+        "AI"
+    ]
+}
+ 
