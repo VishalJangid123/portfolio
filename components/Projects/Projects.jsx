dd28ea2359b0868e918b55662fe6b289debbc403
Vishal Jangid <vishaljangid2@outlook.com>
2025-01-29 01:34:21 +0700

    Removed research tabs and added more project tabs

--- components/Projects/Projects.jsx
+++ components/Projects/Projects.jsx
@@ -1,60 +1,89 @@
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import getProjectMetadata from "../getProjectMetaData";
 import ProjectCard from "./ProjectCard";
+import { ProjectList } from "@/constants/ProjectList";
 
 const Projects = () => {
-    const defaultTabValue = "unity";
-    let matter = getProjectMetadata(defaultTabValue);
-    // "Cube Coordination",
-    const projectList = [
-        "ImageSwift",
-        "Opposites",
-        "Traffic Tango",
-        "Roller Fill",
-        "Drop the Box",
-        "VR Visualization of a Thai Historic temple"
-    ]
-    let final = []
-    projectList.forEach(element => {
-        let newIndex = (matter.findIndex(ele => ele.title == element))
-        final.push(matter[newIndex]);
-    });
-    matter = final;
-    return (
-        <section className="section" id="portfolio">
-            {/* <h2 className="head_title">Portfolio</h2>
-            <span className="head_subtitle"> Projects I had worked on</span> */}
-<div className="h-fit  grid 
-                     lg:container  lg:grid-cols-3 gap-3  sm:grid-cols-1 
-                    sm:place-content-center sm:place-items-center">
-{matter && matter.map(function (item, i) {
-                            return (
-                                <ProjectCard item={item} key={i} />
-                            )
-                        })}
-</div>
-            {/* <div className="flex justify-center mt-16 h-fit">
-                <Tabs defaultValue={defaultTabValue} className="sm:w-screen">
-                    <TabsList>
-                        <TabsTrigger value="unity">Unity</TabsTrigger>
-                        {/* <TabsTrigger value="js_framework">NextJS, ReachJS, NodeJS</TabsTrigger>
-                        <TabsTrigger value="python">Python</TabsTrigger>
-                        <TabsTrigger value="android">Android</TabsTrigger> */}
-                  {/*}  </TabsList>
+  const defaultTabValue = "game_development";
+  let projects = {};
+  projects["full_stack"] = getProjectMetadata("full_stack");
+  projects["game_development"] = getProjectMetadata("game_development");
+  projects["ai"] = getProjectMetadata("ai");
 
-                    <TabsContent value="unity" className="h-fit  grid 
+  Object.keys(projects).forEach((item) => {
+    let final = [];
+    ProjectList[item].forEach((element) => {
+      let newIndex = projects[item].findIndex((ele) => ele.title == element);
+      final.push(projects[item][newIndex]);
+    });
+    projects[item] = final;
+  });
+
+  return (
+    <section className="section" id="portfolio">
+      <h2 className="head_title">Projects</h2>
+      <span className="head_subtitle"> Projects I had worked on</span>
+      {/* <div
+        className="h-fit  grid 
+                     lg:container  lg:grid-cols-3 gap-3  sm:grid-cols-1 
+                    sm:place-content-center sm:place-items-center"
+      >
+        {matter &&
+          matter.map(function (item, i) {
+            return <ProjectCard item={item} key={i} />;
+          })}
+      </div> */}
+      <div className="flex justify-center mt-16 h-fit">
+        <Tabs defaultValue={defaultTabValue} className="sm:w-screen">
+          <TabsList>
+            <TabsTrigger value="game_development">Game Development</TabsTrigger>
+            <TabsTrigger value="full_stack">Full Stack Development</TabsTrigger>
+            <TabsTrigger value="ai">AI</TabsTrigger>
+          </TabsList>
+
+          <TabsContent value="game_development" className="">
+            <div
+              className="h-fit  grid 
                      lg:container  lg:grid-cols-3 gap-4  sm:grid-cols-1 
-                    sm:place-content-center sm:place-items-center">
-                        {matter && matter.map(function (item, i) {
-                            return (
-                                <ProjectCard item={item} key={i} />
-                            )
-                        })}
-                    </TabsContent>
-                </Tabs>
-            </div> */}
-        </section>
-    )
-}
+                    sm:place-content-center sm:place-items-center"
+            >
+              {projects["game_development"] &&
+                projects["game_development"].map(function (item, i) {
+                  return (
+                    <ProjectCard item={item} key={i} tech="game_development" />
+                  );
+                })}
+            </div>
+          </TabsContent>
+          <TabsContent value="full_stack" className="">
+            <div
+              className="h-fit  grid 
+                     lg:container  lg:grid-cols-3 gap-4  sm:grid-cols-1 
+                    sm:place-content-center sm:place-items-center"
+            >
+              {projects["full_stack"] &&
+                projects["full_stack"].map(function (item, i) {
+                  return <ProjectCard item={item} key={i} tech="full_stack" />;
+                })}
+            </div>
+          </TabsContent>
+
+          <TabsContent value="ai" className="">
+            <div
+              className="h-fit  grid 
+                     lg:container  lg:grid-cols-3 gap-4  sm:grid-cols-1 
+                    sm:place-content-center sm:place-items-center"
+            >
+              {projects["ai"] &&
+                projects["ai"].map(function (item, i) {
+                  return <ProjectCard item={item} key={i} tech="ai" />;
+                })}
+            </div>
+          </TabsContent>
+        </Tabs>
+      </div>
+    </section>
+  );
+};
 
 export default Projects;
