3a4ae6f64c0785e7b0a73adec9c9d8c4ef338e07
Vishal Jangid <vishaljangid2@outlook.com>
2025-01-29 23:30:38 +0700

    Updated project list

--- constants/ProjectList.jsx
+++ constants/ProjectList.jsx
@@ -2,7 +2,9 @@ export const ProjectList =
 {
     full_stack : [
         "GoWhere",
-        "Card Management App"
+        "Card Management App",
+        "Object Detection using TensorFlow.js and the COCO-SSD model",
+        "Face Landmark Detection with TensorFlow.js"
     ],
     game_development : [
         "ImageSwift",
@@ -13,7 +15,9 @@ export const ProjectList =
         "VR Visualization of a Thai Historic temple",
     ],
     ai : [
-        "Book Recommender System using ML"
+        "Apply to job email generator",
+        "Book Recommender System using ML",
+        "Yield Prediction"
     ]
 }
  
